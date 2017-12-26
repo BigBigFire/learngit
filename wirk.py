c3ff71d3f0cdc57825e758b4c4cd32bbd83fbc04
BigBigFire <1299730879@qq.com>
2017-12-26 13:05:42 +0800

    ssss

--- wirk.py
+++ wirk.py
@@ -1,8 +1,12 @@
 #!/bin/bash
 print "succ"
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 print "sssssss"
 
+=======
+i = int(raw_input("enter num"))
+>>>>>>> Stashed changes
 =======
 i = int(raw_input("enter num"))
 >>>>>>> Stashed changes
